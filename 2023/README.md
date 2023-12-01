ea50b5a60c1fe86daebbf80bd717f85e134205e0
Michael Tyson <myty@users.noreply.github.com>
2023-12-01 08:17:44 -0500

    completed day 1

---
+++ 2023/README.md
@@ -0,0 +1,53 @@
+# 2023
+
+This project contains solutions to [Advent of Code](https://adventofcode.com/)
+2023, using [Deno](https://deno.land/) and Typescript.
+
+## Usage
+
+You must have [aocd](https://github.com/Macil/aocd) installed and have set your
+session cookie with it:
+
+```
+aocd set-cookie COOKIE_VALUE_HERE
+```
+
+Then you can run any solution script:
+
+```
+deno run -A day_1.ts
+```
+
+You can run one day's tests with `deno test day_1.ts` or by clicking the play
+button next to it inside of Visual Studio Code. You can run all days' tests with
+`deno task test`.
+
+You can debug a script within Visual Studio Code by opening the "Run and Debug"
+view on the left side of Visual Studio Code, picking either the "Debug Current
+File" or "Debug Current File's Tests" configuration in the dropdown, and then
+clicking the play button next to it. You can set breakpoints by clicking to the
+left of a line number to place a red dot.
+
+You can also debug a script outside of Visual Studio Code by running
+`deno run -A --inspect-brk day_1.ts` or `deno test --inspect-brk day_1.ts`.
+
+If you want to use a local file as input for a problem instead of fetching it
+from the Advent of Code website, you can add the `--input` flag to use a
+specific file:
+
+```
+deno run -A day_1.ts --input myInput.txt
+```
+
+When you're confident about a solution, you can add the `--submit` (or `-s`)
+flag to submit the solution and see if it was correct:
+
+```
+deno run -A day_1.ts --submit
+```
+
+You can start a new day's challenge with this command:
+
+```
+aocd start 2
+```
---
+++ 2023/day_1.ts
@@ -0,0 +1,104 @@
+import { assertEquals } from "https://deno.land/std@0.208.0/assert/mod.ts";
+import { runPart } from "https://deno.land/x/aocd@v1.5.1/mod.ts";
+
+const conversionMap = new Map<string, string>(
+  [
+    ["oneight", "18"],
+    ["twone", "21"],
+    ["threeight", "38"],
+    ["fiveight", "58"],
+    ["eightwo", "82"],
+    ["eighthree", "83"],
+    ["nineight", "98"],
+    ["one", "1"],
+    ["two", "2"],
+    ["three", "3"],
+    ["four", "4"],
+    ["five", "5"],
+    ["six", "6"],
+    ["seven", "7"],
+    ["eight", "8"],
+    ["nine", "9"],
+  ],
+);
+
+function convert(input: string): string {
+  conversionMap.forEach((value, key) => {
+    input = input.replaceAll(key, value);
+  });
+
+  return input;
+}
+
+function parse(input: string, shouldConvert = false): number[][] {
+  const numberLines: number[][] = [];
+  const lines = input.trimEnd().split("\n");
+
+  for (const line of lines) {
+    const numbers: number[] = [];
+    const processedLine = shouldConvert ? convert(line) : line;
+
+    for (const char of processedLine) {
+      const numberChar = parseInt(char, 10);
+
+      if (isNaN(numberChar)) {
+        continue;
+      }
+
+      numbers.push(numberChar);
+    }
+
+    numberLines.push(numbers);
+  }
+
+  return numberLines;
+}
+
+function part1(input: string): number {
+  let total = 0;
+
+  for (const line of parse(input)) {
+    const [first] = line;
+    const [last] = line.reverse();
+    const lineNumber = `${first}${last}`;
+
+    total += parseInt(lineNumber, 10);
+  }
+
+  return total;
+}
+
+function part2(input: string): number {
+  let total = 0;
+
+  for (const line of parse(input, true)) {
+    const [first] = line;
+    const [last] = line.reverse();
+    const lineNumber = `${first}${last}`;
+
+    total += parseInt(lineNumber, 10);
+  }
+
+  return total;
+}
+
+if (import.meta.main) {
+  runPart(2023, 1, 1, part1);
+  runPart(2023, 1, 2, part2);
+}
+
+const TEST_INPUT = `\
+6
+7
+8
+9
+10
+`;
+
+Deno.test("part1", () => {
+  assertEquals(part1(TEST_INPUT), 11);
+});
+
+Deno.test("part2", () => {
+  assertEquals(part2(TEST_INPUT), 12);
+});
