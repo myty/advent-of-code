cc593dd1bb19f2fc22ab46e0367152582eee8c8d
Michael Tyson <myty@users.noreply.github.com>
2024-12-10 23:06:42 -0500

    part 1 and 2 submitted

--- 2024/day_10.ts
+++ 2024/day_10.ts
@@ -18,8 +18,7 @@ function parse(input: string) {
   for (let y = 0; y < grid.length; y++) {
     const row = grid[y];
     for (let x = 0; x < row.length; x++) {
-      const node: MapNode = { elevation: row[x], x, y, neighbors: [] };
-      nodes.push(node);
+      const node = neighbors.createMapNode(x, y, row[x]);
 
       neighbors.addEasternNeighbor(node);
       neighbors.addNorthernNeighbor(node);
@@ -31,158 +30,6 @@ function parse(input: string) {
   return nodes;
 }
 
-function getNeighbors(grid: number[][], nodeStore: MapNode[]) {
-  const addEasternNeighbor = (currentNode: MapNode): void => {
-    const { x, y } = currentNode;
-    const row = grid[y];
-    const easternX = x + 1;
-    const easternNodeElevation = row[easternX];
-
-    if (
-      easternNodeElevation == null ||
-      easternNodeElevation > currentNode.elevation + 1 ||
-      easternNodeElevation < currentNode.elevation - 1
-    ) {
-      return;
-    }
-
-    const existingNode = nodeStore.find((node) =>
-      node.x === easternX && node.y === y
-    );
-
-    if (
-      existingNode != null && currentNode.neighbors.indexOf(existingNode) === -1
-    ) {
-      currentNode.neighbors.push(existingNode);
-      return;
-    }
-
-    currentNode.neighbors.push({
-      elevation: row[easternX],
-      x: easternX,
-      y,
-      neighbors: [currentNode],
-    });
-  };
-
-  const addNorthernNeighbor = (currentNode: MapNode): void => {
-    const { x, y } = currentNode;
-    if (y === 0) {
-      return;
-    }
-
-    const northernRowNumber = y - 1;
-    const northernRow = grid[northernRowNumber];
-
-    const northernNodeElevation = northernRow[x];
-    if (
-      northernNodeElevation == null ||
-      northernNodeElevation > currentNode.elevation + 1 ||
-      northernNodeElevation < currentNode.elevation - 1
-    ) {
-      return;
-    }
-
-    const existingNode = nodeStore.find((node) =>
-      node.x === x && node.y === northernRowNumber
-    );
-
-    if (
-      existingNode != null && currentNode.neighbors.indexOf(existingNode) === -1
-    ) {
-      currentNode.neighbors.push(existingNode);
-      return;
-    }
-
-    currentNode.neighbors.push({
-      elevation: northernRow[x],
-      x,
-      y: northernRowNumber,
-      neighbors: [currentNode],
-    });
-  };
-
-  const addSouthernNeighbor = (currentNode: MapNode): void => {
-    const { x, y } = currentNode;
-    if (y === grid.length - 1) {
-      return;
-    }
-
-    const southernRowNumber = y + 1;
-    const southernRow = grid[southernRowNumber];
-
-    const southerNodeElevation = southernRow[x];
-    if (
-      southerNodeElevation == null ||
-      southerNodeElevation > currentNode.elevation + 1 ||
-      southerNodeElevation < currentNode.elevation - 1
-    ) {
-      return;
-    }
-
-    const existingNode = nodeStore.find((node) =>
-      node.x === x && node.y === southernRowNumber
-    );
-    if (
-      existingNode != null && currentNode.neighbors.indexOf(existingNode) === -1
-    ) {
-      currentNode.neighbors.push(existingNode);
-      return;
-    }
-
-    currentNode.neighbors.push({
-      elevation: southernRow[x],
-      x,
-      y: southernRowNumber,
-      neighbors: [currentNode],
-    });
-  };
-
-  const addWesternNeighbor = (currentNode: MapNode): void => {
-    const { x, y } = currentNode;
-    if (x === 0) {
-      return;
-    }
-
-    const westernX = x - 1;
-    const row = grid[y];
-
-    const westernNodeElevation = row[westernX];
-    if (
-      westernNodeElevation == null ||
-      westernNodeElevation > currentNode.elevation + 1 ||
-      westernNodeElevation < currentNode.elevation - 1
-    ) {
-      return;
-    }
-
-    const existingNode = nodeStore.find((node) =>
-      node.x === westernX && node.y === y
-    );
-
-    if (
-      existingNode != null && currentNode.neighbors.indexOf(existingNode) !== -1
-    ) {
-      currentNode.neighbors.push(existingNode);
-      return;
-    }
-
-    currentNode.neighbors.push({
-      elevation: row[westernX],
-      x: westernX,
-      y,
-      neighbors: [currentNode],
-    });
-  };
-
-  return {
-    addNorthernNeighbor,
-    addEasternNeighbor,
-    addSouthernNeighbor,
-    addWesternNeighbor,
-  };
-}
-
 function part1(input: string): number {
   const nodes = parse(input);
 
@@ -192,18 +39,28 @@ function part1(input: string): number {
   }
 
   return startingNodes.reduce((acc, current) => {
-    return acc + getPathsToNineCount(current);
+    const pathsToNine = getPathsFromStartingPoint(current);
+    return acc + pathsToNine;
   }, 0);
 }
 
-// function part2(input: string): number {
-//   const items = parse(input);
-//   throw new Error("TODO");
-// }
+function part2(input: string): number {
+  const nodes = parse(input);
+
+  const startingNodes = nodes.filter((node) => node.elevation === 0);
+  if (startingNodes.length === 0) {
+    throw new Error("No starting nodes found");
+  }
+
+  return startingNodes.reduce((acc, current) => {
+    const pathsToNine = getPathsFromStartingPoint(current, false);
+    return acc + pathsToNine;
+  }, 0);
+}
 
 if (import.meta.main) {
   runPart(2024, 10, 1, part1);
-  // runPart(2024, 10, 2, part2);
+  runPart(2024, 10, 2, part2);
 }
 
 const TEST_INPUT = `\
@@ -221,15 +78,138 @@ Deno.test("part1", () => {
   assertEquals(part1(TEST_INPUT), 36);
 });
 
-function getPathsToNineCount(current: MapNode): number {
-  if (current.elevation === 9) {
-    return 1;
+Deno.test("part2", () => {
+  assertEquals(part2(TEST_INPUT), 81);
+});
+
+function getPathsFromStartingPoint(
+  current: MapNode,
+  shouldDedup = true,
+): number {
+  let nodes = [current];
+
+  for (let i = current.elevation + 1; i <= 9; i++) {
+    const nextNodes = nodes.flatMap((node) =>
+      node.neighbors.filter((n) => n.elevation === i)
+    );
+    if (nextNodes.length === 0) {
+      return 0;
+    }
+
+    nodes = shouldDedup ? dedup(nextNodes) : nextNodes;
   }
 
-  return current.neighbors.reduce((acc, neighbor) => {
-    return acc + getPathsToNineCount(neighbor);
-  }, 0);
+  return nodes.length;
 }
-// Deno.test("part2", () => {
-//   assertEquals(part2(TEST_INPUT), 12);
-// });
+
+function getNeighbors(grid: number[][], nodeStore: MapNode[]) {
+  const createMapNode = (x: number, y: number, elevation: number) => {
+    return nodeStore.find((node) => node.x === x && node.y === y) ??
+      (function () {
+        const node = { x, y, elevation, neighbors: [] };
+        nodeStore.push(node);
+        return node;
+      })();
+  };
+
+  const addMapNodeNeighbor = (
+    currentNode: MapNode,
+    { x, y, elevation }:
+      & Omit<MapNode, "neighbors" | "elevation">
+      & Partial<Pick<MapNode, "elevation">>,
+  ) => {
+    if (elevation == null) {
+      return;
+    }
+
+    const neighborNode = createMapNode(x, y, elevation);
+
+    if (!neighborNode.neighbors.includes(currentNode)) {
+      neighborNode.neighbors.push(currentNode);
+    }
+
+    if (!currentNode.neighbors.includes(neighborNode)) {
+      currentNode.neighbors.push(neighborNode);
+    }
+  };
+
+  const addEasternNeighbor = (currentNode: MapNode): void => {
+    const { x, y } = currentNode;
+    const row = grid[y];
+    const easternX = x + 1;
+
+    addMapNodeNeighbor(currentNode, {
+      elevation: row[easternX],
+      x: easternX,
+      y,
+    });
+  };
+
+  const addNorthernNeighbor = (currentNode: MapNode): void => {
+    const { x, y } = currentNode;
+    if (y === 0) {
+      return;
+    }
+
+    const northernRowNumber = y - 1;
+    const northernRow = grid[northernRowNumber];
+
+    addMapNodeNeighbor(currentNode, {
+      elevation: northernRow[x],
+      x,
+      y: northernRowNumber,
+    });
+  };
+
+  const addSouthernNeighbor = (currentNode: MapNode): void => {
+    const { x, y } = currentNode;
+    if (y === grid.length - 1) {
+      return;
+    }
+
+    const southernRowNumber = y + 1;
+    const southernRow = grid[southernRowNumber];
+
+    addMapNodeNeighbor(currentNode, {
+      elevation: southernRow[x],
+      x,
+      y: southernRowNumber,
+    });
+  };
+
+  const addWesternNeighbor = (currentNode: MapNode): void => {
+    const { x, y } = currentNode;
+    if (x === 0) {
+      return;
+    }
+
+    const westernX = x - 1;
+    const row = grid[y];
+
+    addMapNodeNeighbor(currentNode, {
+      elevation: row[westernX],
+      x: westernX,
+      y,
+    });
+  };
+
+  return {
+    addNorthernNeighbor,
+    addEasternNeighbor,
+    addSouthernNeighbor,
+    addWesternNeighbor,
+    createMapNode,
+  };
+}
+
+function dedup(nextNodes: MapNode[]): MapNode[] {
+  const seen = new Set<MapNode>();
+  return nextNodes.filter((node) => {
+    if (seen.has(node)) {
+      return false;
+    }
+
+    seen.add(node);
+    return true;
+  });
+}
