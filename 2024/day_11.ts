b311ce4786f62d6617cf7c7e5dfc9cedc50257c8
Michael Tyson <myty@users.noreply.github.com>
2024-12-11 20:50:03 -0500

    (day 11 - 2024): part 1 and 2 submitted (#3)
    * part 1 submitted
    
    * part 2 submission
    
    * code cleanup

---
+++ 2024/day_11.ts
@@ -0,0 +1,120 @@
+import { assertEquals } from "@std/assert";
+import { runPart } from "@macil/aocd";
+
+function parse(input: string) {
+  return input.trimEnd().split("\n").filter(Boolean).flatMap((line) =>
+    line.split(" ").filter(Boolean)
+  );
+}
+
+function part1(input: string): number {
+  const numberMap = buildNumberMap(parse(input));
+  return recursiveCount(numberMap, 25);
+}
+
+function part2(input: string): number {
+  const numberMap = buildNumberMap(parse(input));
+  return recursiveCount(numberMap, 75);
+}
+
+if (import.meta.main) {
+  runPart(2024, 11, 1, part1);
+  runPart(2024, 11, 2, part2);
+}
+
+const TEST_INPUT = `125 17`;
+
+Deno.test("part1 - 25", () => {
+  assertEquals(part1(TEST_INPUT), 55312);
+});
+
+Deno.test("part2", () => {
+  assertEquals(part2(TEST_INPUT), 65601038650482);
+});
+
+Deno.test("split", () => {
+  assertEquals(split("1234"), ["12", "34"]);
+  assertEquals(split("1234"), ["12", "34"]);
+  assertEquals(split("72"), ["7", "2"]);
+  assertEquals(split("7200"), ["72", "0"]);
+});
+
+function buildNumberMap(numbers: string[]): Map<string, number> {
+  const numberMap = new Map<string, number>();
+
+  for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+    numberMap.set(number, (numberMap.get(number) ?? 0) + 1);
+  }
+
+  return numberMap;
+}
+
+const cache = new Map<string, string[]>();
+cache.set("0", ["1"]);
+
+function recursiveCount(
+  numbersMap: Map<string, number>,
+  iterationsRemaining: number,
+): number {
+  if (iterationsRemaining === 0) {
+    return numbersMap.values().reduce((sum, count) => sum + count, 0);
+  }
+
+  const entries = Array.from(
+    numbersMap.entries().filter((entry) => {
+      const [_, count] = entry;
+      return (count !== 0);
+    }).map((e) => {
+      const [number, count] = e;
+      return [number, count] as const;
+    }),
+  );
+
+  const nextMap = createMapWrapper(new Map<string, number>());
+
+  for (const [number, prevNumberCount] of entries) {
+    if (cache.has(number)) {
+      for (const cachedNumber of cache.get(number)!) {
+        nextMap.add(cachedNumber, prevNumberCount);
+      }
+      continue;
+    }
+
+    if (number.length % 2 === 0) {
+      const [nextNumberOne, nextNumberTwo] = split(number);
+      cache.set(number, [nextNumberOne, nextNumberTwo]);
+      nextMap.add(nextNumberOne, prevNumberCount);
+      nextMap.add(nextNumberTwo, prevNumberCount);
+      continue;
+    }
+
+    const nextNumber = `${2024 * parseInt(number)}`;
+    cache.set(number, [nextNumber]);
+    nextMap.add(nextNumber, prevNumberCount);
+  }
+
+  return recursiveCount(nextMap.toMap(), iterationsRemaining - 1);
+}
+
+function createMapWrapper(map: Map<string, number>) {
+  return {
+    add: (number: string, count: number) => {
+      map.set(number, (map.get(number) ?? 0) + count);
+    },
+    toMap: () => {
+      return map;
+    },
+  };
+}
+
+function split(number: string): [string, string] {
+  if (number.length % 2 !== 0) {
+    throw new Error("Number must be even length");
+  }
+
+  return [
+    number.slice(0, number.length / 2),
+    `${parseInt(number.slice(number.length / 2))}`,
+  ];
+}
